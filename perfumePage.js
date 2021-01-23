8c719f089402d87d9da94ff7baddc25c4de46daf
amkdr <korotkin.ari@gmail.com>
2021-01-23 15:44:48 -0600

    updated perfumePage function to find all perfume notes

--- perfumePage.js
+++ perfumePage.js
@@ -9,14 +9,39 @@ async function scrapeFragranticaPerfumePage(url) {
         const page = await browser.newPage()
         await page.goto(url)
 
-        // top notes
         await page.waitForXPath('//*[@id="pyramid"]/div[1]/div/div[2]/div[3]/div')
+        await page.waitForXPath('//*[@id="pyramid"]/div[1]/div/div[2]/div[4]/div')
 
+        // top notes
         const topNoteChildren = await page.$$eval('#pyramid > div:nth-child(1) > div > div:nth-child(2) > div:nth-child(4) > div > div', arrOfChildren => {
             return arrOfChildren.map(child => child.textContent)
         })
 
-        console.log(`The number of top notes are ${topNoteChildren.reduce((accum, cur, ind, arr) => {
+        console.log(`The top notes are ${topNoteChildren.reduce((accum, cur, ind, arr) => {
+            if (ind === arr.length - 1) {
+                return accum + 'and ' + cur
+            }
+            return accum + cur + ', '
+        }, '')}.`)
+
+        // middle notes
+        const middleNoteChildren = await page.$$eval('#pyramid > div:nth-child(1) > div > div:nth-child(2) > div:nth-child(6) > div > div', arrOfChildren => {
+            return arrOfChildren.map(child => child.textContent)
+        })
+
+        console.log(`The middle notes are ${middleNoteChildren.reduce((accum, cur, ind, arr)  => {
+            if (ind === arr.length - 1) {
+                return accum + 'and ' + cur
+            }
+            return accum + cur + ', '
+        }, '')}.`)
+
+        // base notes
+        const baseNoteChildren = await page.$$eval('#pyramid > div:nth-child(1) > div > div:nth-child(2) > div:nth-child(8) > div > div', arrOfChildren => {
+            return arrOfChildren.map(child => child.textContent)
+        })
+
+        console.log(`The base notes are ${baseNoteChildren.reduce((accum, cur, ind, arr) => {
             if (ind === arr.length - 1) {
                 return accum + 'and ' + cur
             }
@@ -30,7 +55,7 @@ async function scrapeFragranticaPerfumePage(url) {
     }
 }
 
-scrapeFragranticaPerfumePage('https://www.fragrantica.com/perfume/Yves-Saint-Laurent/Black-Opium-Zebra-Collector-64627.html')
+// scrapeFragranticaPerfumePage('https://www.fragrantica.com/perfume/Yves-Saint-Laurent/Black-Opium-Zebra-Collector-64627.html')
 
-// scrapeFragranticaPerfumePage('https://www.fragrantica.com/perfume/Xerjoff/Lira-11801.html')
+scrapeFragranticaPerfumePage('https://www.fragrantica.com/perfume/Xerjoff/Lira-11801.html')
 
