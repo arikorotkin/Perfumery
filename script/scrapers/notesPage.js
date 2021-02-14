899841529fa83092c24e958456c7253f7e0d63c6
amkdr <korotkin.ari@gmail.com>
2021-02-14 17:24:46 -0600

    created notes page scraper file

---
+++ script/scrapers/notesPage.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer-extra')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+
+puppeteer.use(StealthPlugin())
+
+async function scrapeFragranticaNotePage() {
+    
+}
+
+async function scrapeFragranticaNotesPage() {
+
+}
