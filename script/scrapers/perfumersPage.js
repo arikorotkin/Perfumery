2af1d7fc68402bd3122e8cd1f340752e5989455c
amkdr <korotkin.ari@gmail.com>
2021-02-15 09:00:56 -0600

    chose test urls for perfumer page scraper

--- script/scrapers/perfumersPage.js
+++ script/scrapers/perfumersPage.js
@@ -2,3 +2,12 @@ const puppeteer = require('puppeteer-extra')
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 
 puppeteer.use(StealthPlugin())
+
+// test url 1
+// scrapeFragranticaPerfumerPage('https://www.fragrantica.com/noses/Adriana_Medina-Baez.html')
+
+// test url 2
+// scrapeFragranticaPerfumerPage('https://www.fragrantica.com/noses/Ashley_Eden_Kessler.html')
+
+// test url 3
+// scrapeFragranticaPerfumerPage('https://www.fragrantica.com/noses/Beatrice_Aguilar.html')
