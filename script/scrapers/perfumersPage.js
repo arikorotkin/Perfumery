430a91db3488a5e8dba77ec3225978f1f4a90474
amkdr <korotkin.ari@gmail.com>
2021-02-15 10:56:35 -0600

    commented out test urls in perfumer page scraper

--- script/scrapers/perfumersPage.js
+++ script/scrapers/perfumersPage.js
@@ -62,10 +62,10 @@ async function scrapeFragranticaPerfumersPage() {
 module.exports = scrapeFragranticaPerfumersPage
 
 // test url 1
-scrapeFragranticaPerfumerPage('https://www.fragrantica.com/noses/Adriana_Medina-Baez.html')
+// scrapeFragranticaPerfumerPage('https://www.fragrantica.com/noses/Adriana_Medina-Baez.html')
 
 // test url 2
-scrapeFragranticaPerfumerPage('https://www.fragrantica.com/noses/Ashley_Eden_Kessler.html')
+// scrapeFragranticaPerfumerPage('https://www.fragrantica.com/noses/Ashley_Eden_Kessler.html')
 
 // test url 3
-scrapeFragranticaPerfumerPage('https://www.fragrantica.com/noses/Beatrice_Aguilar.html')
+// scrapeFragranticaPerfumerPage('https://www.fragrantica.com/noses/Beatrice_Aguilar.html')
